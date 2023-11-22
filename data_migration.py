d45cf82555302163ece7e509f11ac497b92796fe
Petro <petrosaliiwork@gmail.com>
2023-11-22 15:07:18 +0200

    Add apis

--- data_migration.py
+++ data_migration.py
@@ -9,21 +9,21 @@ async def main():
     async_session.configure(bind=engine)
     metadata.bind = engine
     async with async_session() as session:
-        with open("scripts/fr-subset.geojsons") as f:
+        with open('json_dump/fr-subset.geojsons') as f:
             objects_to_add = []
             for index, line in enumerate(f):
                 line = json.loads(line)
-                prop = line["properties"]
+                prop = line['properties']
 
                 obj = Field(
-                    id=prop["id"],
-                    crop=prop["crop"],
-                    productivity=prop["productivity"],
-                    area_ha=float(prop["area_ha"]),
-                    history=prop["history"],
-                    region=prop["region"],
-                    score=prop["score"],
-                    coordinates=json.dumps(line["geometry"]),
+                    id=prop['id'],
+                    crop=prop['crop'],
+                    productivity=prop['productivity'],
+                    area_ha=float(prop['area_ha']),
+                    history=prop['history'],
+                    region=prop['region'],
+                    score=prop['score'],
+                    coordinates=json.dumps(line['geometry']),
                 )
                 objects_to_add.append(obj)
                 if index == 200:
@@ -31,6 +31,5 @@ async def main():
             session.add_all(objects_to_add)
             await session.commit()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
